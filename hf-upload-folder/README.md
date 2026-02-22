b71558309c09b4c21a14741316933008c79e6230
vihasreddy58 <vihasreddy111@gmail.com>
2026-02-22 23:00:19 +0530

    Move devcontainer to root

---
+++ hf-upload-folder/README.md
@@ -0,0 +1,6 @@
+﻿---
+title: GA2 Deployment
+sdk: docker
+app_port: 7333
+description: deployment-ready-ga2-b139df
+---
---
+++ hf-upload-folder/main.py
@@ -0,0 +1,8 @@
+﻿from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"status": "deployment-ready-ga2-b139df"}
